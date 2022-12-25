1eaeac8b084c9296a41b4b620264bbbec4b01be9
Ivan Yastrebov <easy.quest@ya.ru>
2022-12-25 17:01:38 +0300

    modified

--- functions/myplugin.fish
+++ functions/myplugin.fish
@@ -1,19 +1,17 @@
 function myplugin -d "My package"
     # Package entry-point
+    fish_info "Hello World!"
+    echo "echo print"
     # Проверьте, нужна ли пользователю помощь.
     if begin
             not set -q argv[1]; or contains -- -h $argv; or contains -- --help $argv
         end
-        test "$package" = -h
-        or test "$package" = --help
-        and set package ""
-
-        myplugin.help "$package"
+        myplugin.help
         return 0
     end
 
-    if test -z "$package" -o "$package" = . -o "$package" = ..
-        echo "Необходимо указать допустимое имя пакета."
-        return 1
-    end
+    # if test -z "$package" -o "$package" = . -o "$package" = ..
+    # echo "Необходимо указать допустимое имя пакета."
+    # return 1
+    # end
 end
